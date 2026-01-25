2cc6fc1dd8d6a8fbe56605d57dd08178a3397d04
UdaraChamidu <chamiduudara321@gmail.com>
2026-01-25 14:06:11 +0530

    fix: deployment error

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -51,6 +51,7 @@ class APIClient {
       ...options,
       headers: {
         'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
         ...options.headers,
       },
     });
